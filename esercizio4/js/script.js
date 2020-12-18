4ecfdbab9276a6a03fcf3fa84ea0989bb534fbe0
Fede <chiellofederico@gmail.com>
2020-12-18 15:28:38 +0100

    fine esercizio 4

--- esercizio4/js/script.js
+++ esercizio4/js/script.js
@@ -1,4 +1,19 @@
 // Istruzioni:
 // l’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 
-var numero = parseInt(prompt('Inserisci un numero'));
+// Variabili
+var primoNumero = parseFloat(prompt('Inserisci un numero:'));
+var secondoNumero = parseFloat(prompt('Inserisci un altro numero:'));
+var numeroMaggiore = 'Numeri uguali';
+
+// Condizioni
+// Deve essere stampato il maggiore tra i due numeri.
+if (primoNumero > secondoNumero) {
+  numeroMaggiore = 'Il numero maggiore è: ' + primoNumero;
+  // console.log(numeroMaggiore);
+} else if(secondoNumero > primoNumero) {
+  numeroMaggiore = 'Il numero maggiore è: ' + secondoNumero;
+  // console.log(numeroMaggiore);
+}
+
+document.getElementById('risultato').innerHTML += numeroMaggiore;
