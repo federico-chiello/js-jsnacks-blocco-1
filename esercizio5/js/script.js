a93ec012ab2a316b8433aea9669b805466b8b975
Fede <chiellofederico@gmail.com>
2020-12-18 15:59:19 +0100

    fine esercizio 5

--- esercizio5/js/script.js
+++ esercizio5/js/script.js
@@ -1,2 +1,23 @@
 // Istruzioni:
 // l’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+// Variabili
+var primaParola = prompt('Inserisci una parola:');
+var secondaParola = prompt('Inserisci un\'altra parola:');
+var letteraIniziale = primaParola.charAt(0).toUpperCase();
+var restoParola = primaParola.substring(1).toLowerCase();
+var primaLettera = secondaParola.charAt(0).toUpperCase();
+var restoLettere = secondaParola.substring(1).toLowerCase();
+primaParola = letteraIniziale +  restoParola;
+secondaParola = primaLettera +  restoLettere;
+var risultatoUno = primaParola + ' ' + secondaParola;
+var risultatoDue = secondaParola + ' ' + primaParola;
+
+// Condizione
+// stampare prima la parola corta, poi quella lunga.
+if (primaParola > secondaParola) {
+  console.log(risultatoDue);
+} else if (secondaParola > primaParola) {
+  console.log(risultatoUno);
+}
