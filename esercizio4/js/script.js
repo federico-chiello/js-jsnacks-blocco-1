6f07a55a49ccce72c5f5b5abb7c44ac38e3b55c3
Fede <chiellofederico@gmail.com>
2020-12-18 15:30:14 +0100

    aggiunta numeroFinale

--- esercizio4/js/script.js
+++ esercizio4/js/script.js
@@ -4,16 +4,16 @@
 // Variabili
 var primoNumero = parseFloat(prompt('Inserisci un numero:'));
 var secondoNumero = parseFloat(prompt('Inserisci un altro numero:'));
-var numeroMaggiore = 'Numeri uguali';
+var numeroFinale = 'Numeri uguali';
 
 // Condizioni
 // Deve essere stampato il maggiore tra i due numeri.
 if (primoNumero > secondoNumero) {
-  numeroMaggiore = 'Il numero maggiore è: ' + primoNumero;
+  numeroFinale = 'Il numero maggiore è: ' + primoNumero;
   // console.log(numeroMaggiore);
 } else if(secondoNumero > primoNumero) {
-  numeroMaggiore = 'Il numero maggiore è: ' + secondoNumero;
+  numeroFinale = 'Il numero maggiore è: ' + secondoNumero;
   // console.log(numeroMaggiore);
 }
 
-document.getElementById('risultato').innerHTML += numeroMaggiore;
+document.getElementById('risultato').innerHTML = numeroFinale;
