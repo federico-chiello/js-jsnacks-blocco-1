14e59ee024333eb174dd40ac71578afcb14d84c7
Fede <chiellofederico@gmail.com>
2020-12-18 12:31:23 +0100

    fine esercizio 3

--- esercizio3/js/script.js
+++ esercizio3/js/script.js
@@ -1,16 +1,15 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 
+var i = 0;
+var invitatiFesta = ['Rossi', 'Verdi', 'Bianchi', 'Neri'];
+var esito = 'Non puoi partecipare';
+var utente = prompt('Inserisci il tuo nome');
 
-
-for (var i = 0; i <=0; i++) {
-  var invitatiFesta = ['Rossi', 'Verdi', 'Bianchi', 'Neri'];
-  var utente = prompt('Inserisci il tuo nome');
-  if (utente != invitatiFesta){
-    utente = 'Non sei tra gli invitati';
-  } else {
-    utente = 'Puoi partecipare';
+for (i = 0; i < invitatiFesta.length; i++) {
+  if (utente == invitatiFesta[i]){
+    esito = 'Puoi partecipare';
   }
 }
 
-document.getElementById('festa').innerHTML = utente;
+document.getElementById('festa').innerHTML = esito;
